323ff7aeb054890b024642a51ad412c14ab5c9b5
Dylan Palmboom <nofxboy1234@gmail.com>
2024-09-22 21:11:14 +0200

    Remove unused actions from CharactersController

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,6 @@
 class CharactersController < ApplicationController
   include JsonWebToken
 
-  before_action :set_character, only: %i[ show update destroy ]
-
   # GET /characters
   def index
     @characters = Character.all
@@ -59,40 +57,4 @@ class CharactersController < ApplicationController
       render json: { found: false, name: character.name, all_found: false }, status: :ok
     end
   end
-
-  # POST /characters
-  def create
-    @character = Character.new(character_params)
-
-    if @character.save
-      render json: @character, status: :created, location: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /characters/1
-  def update
-    if @character.update(character_params)
-      render json: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /characters/1
-  def destroy
-    @character.destroy!
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_character
-      @character = Character.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def character_params
-      params.require(:character).permit(:name, :position)
-    end
 end
